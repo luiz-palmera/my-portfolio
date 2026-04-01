de33647c7c40a676563c6cb63623ea71c08a2607
Luiz <luizrdamata@gmail.com>
2026-04-01 17:58:51 -0300

    feat: create hero page

---
+++ src/pages/Hero.tsx
@@ -0,0 +1,9 @@
+import Navbar from "@/components/navbar";
+
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center h-screen">
+      <Navbar />
+    </div>
+  );
+}
