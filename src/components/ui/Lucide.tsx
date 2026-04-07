faf3449fcf2ba20b8d61142adc5379b38538abe0
Luiz <luizrdamata@gmail.com>
2026-04-07 11:04:03 -0300

    feat: add Lucide component to improve icons usage

---
+++ src/components/ui/Lucide.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utlis";
+import * as lucideIcons from "lucide-react";
+import { twMerge } from "tailwind-merge";
+
+export const { icons } = lucideIcons;
+
+export interface LucideProps extends React.ComponentPropsWithoutRef<"svg"> {
+  icon: keyof typeof icons;
+  title?: string;
+}
+
+function Lucide(props: LucideProps) {
+  const { icon, className, ...computedProps } = props;
+  const Component = icons[icon];
+  return (
+    <Component
+      {...computedProps}
+      className={cn("stroke-1.5 w-5 h-5", className)}
+    />
+  );
+}
+
+export default Lucide;
