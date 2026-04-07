702d755b4c60a6afa5161a8042b37fe3be9341b2
Luiz <luizrdamata@gmail.com>
2026-04-07 10:17:50 -0300

    feat(style): create navwrapper component

---
+++ src/components/navbar/components/navwrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { motion } from "motion/react";
+
+export default function NavWrapper({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <motion.div
+      initial={{ y: "-50vh" }}
+      animate={{ y: 0 }}
+      transition={{
+        type: "spring",
+        stiffness: 120,
+        damping: 11,
+      }}
+      className={
+        "rounded-2xl gap-6 flex items-center bg-background-secondary justify-center px-7 py-3 " +
+        className
+      }
+    >
+      {children}
+    </motion.div>
+  );
+}
